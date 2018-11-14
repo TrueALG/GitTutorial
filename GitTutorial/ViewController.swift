c6732bd755223c5393fd956c53e76f2fc2b29128
Ciriaco, Thiago A <216940@s020rmc148-08.local>
2018-11-14 09:48:36 -0600

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Ciriaco, Thiago A on 11/14/18.
+//  Copyright © 2018 Ciriaco, Thiago A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
