ac0a01c2ff2317a26d34c3bc2d505a394f967aa5
Ciriaco, Thiago A <216940@s020rmc148-08.local>
2018-11-16 09:43:55 -0600

    Added file and class MapForItinerary that inherits from NSObject

---
+++ MapForItinerary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItinerary.swift
+//  GitTutorial
+//
+//  Created by Ciriaco, Thiago A on 11/16/18.
+//  Copyright © 2018 Ciriaco, Thiago A. All rights reserved.
+//
+
+import Foundation
+class MapForItinerary : NSObject {
+    func sayHello() {
+        print("Hello from MapForItinerary")
+    }
+}
